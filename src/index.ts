e76ed464ffb89c7a7b4df6201b37f18d4a5525cc
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2023-01-29 19:44:31 +0300

    fix: return http

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import https from 'https';
-import * as fs from 'fs';
+import http from 'http';
 import { Server, Socket } from 'socket.io';
 
 import corsResolver from './corsResolver';
@@ -22,13 +21,7 @@ app.use(corsResolver);
 app.use('/users', usersRouter);
 app.use('/messages', messagesRouter);
 
-const server = https.createServer(
-  {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem'),
-  },
-  app
-);
+const server = http.createServer(app);
 
 const io = new Server<
   ClientToServerEvents,
