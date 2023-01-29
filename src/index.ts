e70d94880fdfbc72ca1129fdff2a0132cfd729af
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2023-01-29 15:19:58 +0300

    feat: add sockets

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import http from 'http';
+import { Server } from 'socket.io';
 
 import corsResolver from './corsResolver';
 import messagesRouter from './routers/messagesRouter';
@@ -13,12 +15,28 @@ app.use(corsResolver);
 app.use('/users', usersRouter);
 app.use('/messages', messagesRouter);
 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  socket.on('send_message', (data) => {
+    socket.broadcast.emit('receive_message', data);
+  });
+});
+
 const start = async () => {
   try {
     await mongoose.connect(
       'mongodb+srv://naztya12323:268bsn@cluster0.heqc0oe.mongodb.net/email-backend?retryWrites=true&w=majority'
     );
-    app.listen(PORT, () => console.log('server started'));
+    server.listen(PORT, () => console.log('server started'));
   } catch (error) {
     throw new Error(`${error}`);
   }
